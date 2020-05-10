377762dcc1fa30297faf52fff24b55cbe06e5188
Johnny <Johnny@DESKTOP-31T0HM6>
2020-05-10 05:16:13 -0400

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSharp.Net_FinalProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Resets all combobox selected items to null
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            List<ComboBox> cbList = new List<ComboBox>();
+            cbList.Add(cb_CPU);
+            cbList.Add(cb_GPU);
+            cbList.Add(cb_MB);
+            cbList.Add(cb_RAM);
+            cbList.Add(cb_SSDHDD);
+            foreach (ComboBox cb in cbList)
+            {
+                cb.SelectedItem = null;
+            }
+        }
+
+        // Add Part Button
+        private void AddPart_Click(object sender, RoutedEventArgs e)
+        {
+            AddPart addPart = new AddPart();
+            addPart.Show();
+        }
+
+        // Edit Part Button
+        private void EditPart_Click(object sender, RoutedEventArgs e)
+        {
+            EditPart editPart = new EditPart();
+            editPart.Show();
+        }
+
+        // Remove Part Button
+        private void RemovePart_Click(object sender, RoutedEventArgs e)
+        {
+            RemovePart removePart = new RemovePart();
+            removePart.Show();
+        }
+    }
+}
